612f863e93b6470e5bdc5f09a1fe1051624425bf
PS <piotr.sroka@outlook.com>
2023-03-29 22:41:24 +0200

    2-10

---
+++ 2-10.py
@@ -0,0 +1,19 @@
+options =  ['load data', 'export data', 'analyze & predict']
+choice = 'x'
+
+def DisplayOptions(options):
+    for index, value in enumerate(options,start=1):
+        print(index, value)
+    return input('Select option above or press enter to exit: ')
+    
+
+while choice:
+    choice = DisplayOptions(options)
+    if choice:
+        try:
+            choice_num = int(choice)
+            print(options[choice_num-1]) if (choice_num and (choice_num <= len(options))) else print(f'Dokonano niewłaściwego wyboru')
+        except:
+            print(f'Wartość powinna być liczbą')
+    else:
+        print(f'Program zakończył działanie')
